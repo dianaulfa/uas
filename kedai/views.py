2817588108f386ea1b19300460768d92db880e90
unknown <ulfad9347@gmail.com>
2023-12-29 09:34:23 +0700

    push yg ke-1

---
+++ kedai/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return render(request, 'kedai/index.html')
+
+def menu(request):
+    return render(request, 'kedai/menu.html')
---
+++ mysite/urls.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from django.urls import path
+from kedai.views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', index, name='index'),
+    path('menu/', menu, name='menu'),
+]
